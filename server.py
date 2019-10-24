f64f70ba2240383ffc21d5aab0c608df636d9db4
ogzgbkn <gogebakanoguz4@gmail.com>
2019-10-24 15:46:31 +0300

    Hello world changed

--- server.py
+++ server.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_page():
-    return "Hello, world!"
+    return "My movie collection"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000, debug=True)
