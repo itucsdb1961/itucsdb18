8b55ee7f3858abf82984300abce055876c3aaef8
ogzgbkn <gogebakanoguz4@gmail.com>
2019-10-24 17:10:47 +0300

    Minor changes

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_page():
-    return "My movie collection"
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
