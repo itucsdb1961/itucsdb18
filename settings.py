392f5520659c70f5939b87219965cbb3c70b8041
salihfc <salihfc99@gmail.com>
2019-12-15 13:52:08 +0300

    trying clean build

--- settings.py
+++
@@ -1,4 +0,0 @@
-DEBUG = True
-PORT = 8000
-DATABASE_URL = "postgres://vzvhmhqevlcedf:141b03607dee6c5c995d91b952b06e4fc122006f5cd2c1d789403aae34dc40a1@ec2-54-217-225-16.eu-west-1.compute.amazonaws.com:5432/dafo7esm4hjfc7"
-SECRET_KEY = "hjkalsfdlamfrqwrxzc"
