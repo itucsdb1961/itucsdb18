4d4fddf417c4378730aaca66f4995f0d0e1b3eb9
salihfc <salihfc99@gmail.com>
2019-12-15 10:48:52 +0300

    seperated view function into files

---
+++ author_view.py
@@ -0,0 +1,157 @@
+url = "postgres://vzvhmhqevlcedf:141b03607dee6c5c995d91b952b06e4fc122006f5cd2c1d789403aae34dc40a1@ec2-54-217-225-16.eu-west-1.compute.amazonaws.com:5432/dafo7esm4hjfc7"
+secret_key = "hjkalsfdlamfrqwrxzc"
+import psycopg2 as dbapi2
+from flask import Flask, request, redirect, url_for,render_template
+
+class author:
+	def __init__(self,
+				name,
+				last_name,
+				birth_year = None,
+				birth_place = None,
+				last_book_date = None,
+				last_book_name = None,
+				):
+		self.name = name
+		self.last_name = last_name
+		self.birth_year = birth_year
+		self.birth_place = birth_place
+		self.last_book_date = last_book_date
+		self.last_book_name = last_book_name
+
+	def add_to_db(self,db_url):
+		STATEMENT ='''
+					INSERT INTO
+					AUTHORS	(NAME, LAST_NAME, BIRTH_YR, BIRTH_PLACE, LAST_BOOK_DATE, LAST_BOOK_NAME)
+					VALUES 	('%s', '%s', '%s', '%s', '%s', '%s')					
+					ON CONFLICT(NAME, LAST_NAME) DO NOTHING
+					''' % (self.name, self.last_name, self.birth_year, self.birth_place, self.last_book_date, self.last_book_name)
+
+		with dbapi2.connect(db_url) as connection:
+			cursor = connection.cursor()
+			cursor.execute(STATEMENT)
+			connection.commit()
+	
+	def fetch_id(self,db_url):
+		
+		with dbapi2.connect(db_url) as connection:
+			cursor = connection.cursor()
+			cursor.execute(
+					'''
+					select * from authors
+					where 
+					NAME = '%s' and
+					LAST_NAME = '%s'
+					''' % (self.name, self.last_name)
+					)
+			ids = cursor.fetchall()
+			return ids[0][0]
+
+		
+def admin_authors_page():
+
+	if request.method == "POST":
+		tmp_author = author(request.form["author_name"], request.form["last_name"] ,request.form["birth_year"], request.form["birth_place"], request.form["last_book_date"], request.form["last_book_name"])
+		tmp_author.add_to_db(url)
+
+	authors = []
+
+	with dbapi2.connect(url) as connection:
+		cursor = connection.cursor()
+		cursor.execute("select * from authors")
+		authors = cursor.fetchall()
+	return render_template("authors.html", authors = authors)
+	
+def author_page(author_id):
+	
+	author = []
+	books = []
+	
+	statement_author = '''
+		SELECT * FROM AUTHORS
+		WHERE (ID = %d)
+	''' % (int(author_id))
+	
+	statement_books = '''
+		SELECT * FROM BOOK_AUTHORS
+		WHERE (AUTHOR_ID = %d)
+	''' % (int(author_id))
+	
+	with dbapi2.connect(url) as connection:
+			cursor = connection.cursor()
+			cursor.execute(statement_author)
+			author = cursor.fetchall()
+			
+			cursor.execute(statement_books)
+			books = cursor.fetchall()
+			
+
+	return render_template("author.html", author = author, books = books)
+'''
+	if request.method == "GET":
+		return render_template("admin_authors.html", authors = authors)
+	else:
+		
+		with dbapi2.connect(url) as connection:
+			cursor = connection.cursor()
+			cursor.execute(statement)
+			authors = cursor.fetchall()
+
+		return render_template("admin_authors.html", author = author)
+'''
+
+def authors_page():
+
+	authors = []
+
+	with dbapi2.connect(url) as connection:
+		cursor = connection.cursor()
+		cursor.execute("select * from authors")
+		authors = cursor.fetchall()
+
+	if request.method == "GET":
+		print(authors)
+		return render_template("authors.html", authors = authors)
+  
+	with dbapi2.connect(url) as connection:
+		cursor = connection.cursor()
+		cursor.execute(statement_author)
+		author = cursor.fetchall()
+		
+		cursor.execute(statement_books)
+		author_book = cursor.fetchall() # return book ids
+				
+		for row in author_book:
+			print("row= ")
+			print(row)
+			book_id = row[0]
+				
+			print("book_id = " + str(book_id))
+					
+			statement_book_ids = '''
+				SELECT * FROM BOOKS
+				WHERE (ID = %d)
+			''' % (int(book_id))
+			
+			cursor.execute(statement_book_ids)
+			
+			book = cursor.fetchall()
+			
+			print(book)			
+			books.append(book)
+		
+	print("books = " + str(books))
+	return render_template("author.html", author = author, books = books)	
+
+def delete_author(author_id):
+	
+	statement = '''
+		DELETE FROM AUTHORS
+		WHERE (ID = %d)
+	''' % (int(author_id))
+	
+	with dbapi2.connect(url) as connection:
+		cursor = connection.cursor()
+		cursor.execute(statement)
+	
+	return redirect(url_for("authors_page"))
--- server.py
+++ server.py
@@ -1,13 +1,13 @@
-from flask import Flask
+url = "postgres://vzvhmhqevlcedf:141b03607dee6c5c995d91b952b06e4fc122006f5cd2c1d789403aae34dc40a1@ec2-54-217-225-16.eu-west-1.compute.amazonaws.com:5432/dafo7esm4hjfc7"
+secret_key = "hjkalsfdlamfrqwrxzc"
+
+from flask import Flask, request, redirect, url_for
 import os
 import views
 import psycopg2 as dbapi2
 from dbinit import init_db
-from flask import request, redirect, url_for
+import book_view, author_view, student_view
 
-
-url = "postgres://vzvhmhqevlcedf:141b03607dee6c5c995d91b952b06e4fc122006f5cd2c1d789403aae34dc40a1@ec2-54-217-225-16.eu-west-1.compute.amazonaws.com:5432/dafo7esm4hjfc7"
-secret_key = "hjkalsfdlamfrqwrxzc"
 app = Flask(__name__)
 
 def create_app():
@@ -16,25 +16,25 @@ def create_app():
 
 	app.add_url_rule("/", view_func=views.home_page)
   
-	app.add_url_rule("/admin/books", view_func=views.admin_books_page , methods=["GET", "POST"])
+	app.add_url_rule("/admin/books", view_func=book_view.admin_books_page , methods=["GET", "POST"])
 	
-	app.add_url_rule("/books", view_func=views.books_page , methods=["GET", "POST"])
-	app.add_url_rule("/book/<book_id>", view_func = views.book_page)
-	app.add_url_rule("/book/delete/<book_id>", view_func = views.delete_book)
+	app.add_url_rule("/books", view_func=book_view.books_page , methods=["GET", "POST"])
+	app.add_url_rule("/book/<book_id>", view_func = book_view.book_page)
+	app.add_url_rule("/book/delete/<book_id>", view_func = book_view.delete_book)
 
-	app.add_url_rule("/author/<author_id>", view_func = views.author_page)
-	app.add_url_rule("/author/delete/<author_id>", view_func = views.delete_author)
+	app.add_url_rule("/author/<author_id>", view_func = author_view.author_page)
+	app.add_url_rule("/author/delete/<author_id>", view_func = author_view.delete_author)
 
-	app.add_url_rule("/admin/authors", view_func=views.admin_authors_page)
-	app.add_url_rule("/authors", view_func=views.authors_page, methods=["GET", "POST"]))
+	app.add_url_rule("/admin/authors", view_func=author_view.admin_authors_page)
+	app.add_url_rule("/authors", view_func=author_view.authors_page, methods=["GET", "POST"])
 
 	app.add_url_rule("/admin/closets", view_func=views.admin_closets_page, methods=["GET", "POST"])
 	app.add_url_rule("/closets", view_func=views.closets_page, methods=["GET", "POST"])
 
-  app.add_url_rule("/login", view_func=views.admin_login_page, methods=["GET", "POST"])
+	app.add_url_rule("/login", view_func=views.admin_login_page, methods=["GET", "POST"])
 
-	app.add_url_rule("/admin/students", view_func=views.admin_students, methods=["GET", "POST"])
-	app.add_url_rule("/admin/student/<student_id>", view_func=views.admin_student, methods=["GET", "POST"])
+	app.add_url_rule("/admin/students", view_func=student_view.admin_students, methods=["GET", "POST"])
+	app.add_url_rule("/admin/student/<student_id>", view_func=student_view.admin_student, methods=["GET", "POST"])
 
 	return app
 
