c3d6c6dd84f90948a21a920b500fb4317c6d6fc1
salihfc <salihfc99@gmail.com>
2019-12-16 16:58:34 +0300

    app global-2

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 DEBUG = True
-PORT = 8000
+PORT = 5000
 DATABASE_URL = "postgres://mrzogiikkbrxmf:b6042668a00f9ea7e4d353f06e02e8c5ffab90a6a2a76191de8597340a254d68@ec2-54-228-243-29.eu-west-1.compute.amazonaws.com:5432/dsbe8b5jahoaq"
 SECRET_KEY = "hjkalsfdlamfrqwrxzc"
